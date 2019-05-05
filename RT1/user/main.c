bfac09c606a9cf0a44d6366035113409c2e7b3f2
zhr127 <44157571+zhr127@users.noreply.github.com>
2019-05-06 01:26:24 +0800

    Create main.c

---
+++ RT1/user/main.c
@@ -0,0 +1,19 @@
+ int flag1 = 0;
+ int flag2 = 0;
+ int flag3 = 0;
+
+int main(void)
+{
+  /* */  
+	HardWareInit();
+	/*  */  
+	for( ; ;) {
+	     
+		 DoSomething1();
+		 
+		 DoSomething2();
+		 
+		 DoSomething3();
+	
+	}
+}
