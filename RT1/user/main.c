c28984552f9937abc1a00a9a6e3d21f3490a82ea
zhr127 <44157571+zhr127@users.noreply.github.com>
2019-05-07 18:32:53 +0800

    Update main.c

--- RT1/user/main.c
+++ RT1/user/main.c
@@ -112,6 +112,86 @@ rt_err_t rt_thread_init(struct rt_thread *thread,
 							rt_list_init(&(thread->tlist));
 							
 							thread->entry = (void *)entry;
+							thread->parameter = parameter;
 							
+							thread->stack_addr = stack_start;
+							thread->stack_size = stack_size;
+							
+							thread->sp =
+							(void *)rt_hw_stack_init( thread->entry,
+							                          thread->parameter,
+							(void *)((char *)thread->stack_addr + thread->stack_size - 4));
+							
+							return RT_EOK;
 						}
 
+struct rt_list_node
+{
+	struct rt_list_node *next;
+	struct rt_list_node *prev;
+};
+typedef struct rt_list_node rt_list_t;
+
+rt_inline void rt_list_init(rt_list_t *l)
+{
+	l->next = l->prev = l;
+}
+
+rt_inline void rt_list_insert_after(rt_list_t *l, rt_list_t *n)
+{
+	l->next->prev = n;
+	n->next = l->next;
+	
+	l->next = n;
+	n->prev = l;
+}
+
+rt_inline void rt_list_insert_before(rt_list_t *l, rt_list_t *n)
+{
+	l->prev->next = n;
+	n->prev = l->prev;
+	
+	l->prev = n;
+	n->next = l;
+}
+
+rt_inline void rt_list_remove(rt_list_t *n)
+{
+	n->next->prev = n->prev;
+	n->prev->next = n->next;
+	
+	n->next = n->prev = n;
+}
+
+rt_uint8_t *rt_hw_stack_init(void  *tentry,
+                             void  *parameter,
+							 rt_unint8_t *stack_addr)
+{
+	struct stack_frame *stack_frame;
+	rt_uint8_t         *stk;
+	unsigned long        i;
+	
+	stk = stack_addr + sizeof(rt_uint32_t);
+	
+	stk = (rt_uint8_t *)RT_ALIGN_DOWN((rt_uint32_t)stk, 8);
+	
+	stk -= sizeof(struct stack_frame);
+	
+	stack_frame = (struct stack_frame *)stk;
+	
+	for (i = 0; i < sizeof(struct stack_frame) / sizeof(rt_uint32_t); i++)
+	{
+		((rt_uint32_t *)stack_frame)[i] = 0xdeadbeef;
+	}
+	
+	stack_frame->execption_stack_frame.r0 = (unsigned long)parameter;
+	stack_frame->execption_stack_frame.r1 = 0;
+	stack_frame->execption_stack_frame.r2 = 0;
+	stack_frame->execption_stack_frame.r3 = 0;
+	stack_frame->execption_stack_frame.r12 = 0;
+	stack_frame->execption_stack_frame.lr = 0;
+	stack_frame->execption_stack_frame.pc = (unsigned long)tentry;
+	stack_frame->execption_stack_frame.psr = 0x01000000L;
+	
+	return stk;
+}
